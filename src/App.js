a8b369ade5209ddb6c6847f68100d83ab6f24af7
Irwanto <irwantojo@gmail.com>
2020-04-23 13:23:42 +0700

    test copy and edit app

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now!!
+          Learn travis and aws beanstalk
         </a>
       </header>
     </div>
